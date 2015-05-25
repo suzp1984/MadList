9e8e8e280a9e3955c7098d5626aa44a055e57e52
suzp1984 <suzp1984@gmail.com>
2015-05-25 16:02:45 +0800

    Add Reverse List Activity

---
+++ app/src/main/java/org/zpcat/madlist/ReverseListActivity.java
@@ -0,0 +1,53 @@
+package org.zpcat.madlist;
+
+import android.app.ListActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+
+public class ReverseListActivity extends ListActivity {
+
+    private String[] mStrings = {
+            "Bordeaux",
+            "Lyon",
+            "Marseille",
+            "Nancy",
+            "Paris",
+            "Toulouse",
+            "Strasbourg"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reverse_list);
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mStrings));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_reverse_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
